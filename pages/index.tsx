792d1311534db96b1aa2852cbe269f66d49e4e62
August <shoislomshobaxromov@gmail.com>
2022-05-16 13:40:51 +0500

    removed unneccary things

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import { useAppDispatch, useAppSelector } from "../src/redux/hooks";
 import styles from "../styles/Home.module.css";
 import { Box, Button, Typography } from "@mui/material";
